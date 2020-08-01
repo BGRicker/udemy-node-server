65c8d85b53ab1bc11f3fc4e1bf4f1bcd1f2c6569
Ben Ricker <bgricker@bgricker.com>
2020-08-01 13:11:55 -0400

    mongoose connection

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-require('./services/passport');
-
 const app = express();
-
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+mongoose.connect(keys.mongoURI);
+require('./services/passport');
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
