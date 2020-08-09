faeb585939961aa0d07bec7f056b0783975f1d97
Ben Ricker <bgricker@bgricker.com>
2020-08-09 17:43:54 -0400

    Header component, materialize setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,21 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-const Header = () => <h2>Header</h2>;
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <BrowserRouter>
         <div>
+          <Header></Header>
           <Route exact path="/" component={Landing}></Route>
-          <Route path="/dashboard" component={Dashboard}></Route>
-          <Route path="/surveys" component={SurveyNew}></Route>
+          <Route exact path="/surveys" component={Dashboard}></Route>
+          <Route path="/surveys/new" component={SurveyNew}></Route>
         </div>
       </BrowserRouter>
     </div>
